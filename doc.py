1ebc3df06e5a6f2d5ce8726aa1e983f3dee4b61e
End Point <incsrc@aol.com>
2011-09-25 01:37:39 +0200

    save pre->object <-> document relation

--- doc.py
+++ doc.py
@@ -1,3 +1,6 @@
+from hashlib import sha1
+from rediskeys import *
+
 class Document(object):
     def __init__(self, id = 0):
         self.id = int(id)
@@ -7,3 +10,9 @@ class Document(object):
         else:
             self.content = False
 
+    def add_relation(self, redis, pre, object):
+        obj_hash = sha1(object).hexdigest()
+        print obj_hash
+        obj_id = redis.get(OBJ_ID % (pre, obj_hash))
+
+        redis.zincrby(DOC_RELATED % (self.id, pre), obj_id)
