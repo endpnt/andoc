150f2be57e0b68dc029a119ca200dd62e0c209a9
End Point <incsrc@aol.com>
2011-09-25 19:32:56 +0200

    create gml, create edges, calc weight

--- featext.py
+++ featext.py
@@ -12,8 +12,6 @@ from itertools import combinations
 import igraph
 
 redis = Redis()
-g = igraph.Graph()
-
 pipe = redis.pipeline()
 
 # load all document ids
@@ -31,37 +29,47 @@ object_score = dict()
 doc_obj_map = dict()
 for result_set in tmp_obj_score:
     docid = document_ids.pop(0)
-    doc_obj_map[int(docid)] = set( [ int(k) for k,v in result_set ])
-    object_score.update([ (int(k),v) for k,v in result_set])
+    doc_obj_map[int(docid)] = set( [ int(k) - 1 for k,v in result_set ])
+    object_score.update([ (int(k) - 1 ,v) for k,v in result_set])
 
 # load all values for each obj_id
 for obj_id in object_score.iterkeys():
     pipe.get(OBJECT_VALUE % ('person',obj_id))
 
 tmp_obj_value = pipe.execute()
+print "%s obj values in redis" % len(tmp_obj_value)
 
 # load all object values 
 object_value = dict()
 for obj_id in object_score.iterkeys():
-    object_value[obj_id] = tmp_obj_value.pop(0)
+    object_value[obj_id] = tmp_obj_value.pop(0) 
 
-print doc_obj_map
-print object_score
-print object_value
+print "%s docs" % len(doc_obj_map)
+print "%s scores" % len(object_score)
+print "%s values" % len(object_value)
 
 # create a relation between objects in the same document
 obj_rel_map = list()
 for obj_ids in doc_obj_map.itervalues():
     obj_rel_map.extend([ v for v in combinations(obj_ids,2) ])
 
-print obj_rel_map
+print "%s obj rel" % len(obj_rel_map)
 
 # count the relations over all documents
 obj_rel_count = [ [e, obj_rel_map.count(e)] for e in set(obj_rel_map) ]
 
-print obj_rel_count
+print "%s obj count" % len(obj_rel_count)
 
 # finally split relation (edges) and count (weight)
 edges, weight = map(list, zip(*obj_rel_count))
 
-print edges, weight
+print "%s edges" % len(edges) 
+print "%s weight" % len(weight)
+
+g = igraph.Graph(edges)
+g.es['weight'] = weight
+g.vs['label'] = object_value.values()
+g.vs['score'] = object_score.values()
+
+print g
+g.save('data/person.gml')
