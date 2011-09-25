bceeeffd63d71daba7518bdf4794c1abc85d5691
End Point <incsrc@aol.com>
2011-09-25 16:37:54 +0200

    feature extraction and relation of objects

--- featext.py
+++ featext.py
@@ -8,6 +8,7 @@ from rediskeys import *
 from redis import Redis
 from urlparse import urlsplit
 from sys import exit
+from itertools import combinations
 import igraph
 
 redis = Redis()
@@ -24,8 +25,9 @@ for docid in document_ids:
 
 tmp_obj_score = pipe.execute()
 
-# save obj and score into a dict
+# save obj and score 
 object_score = dict()
+# save doc -> obj relation
 doc_obj_map = dict()
 for result_set in tmp_obj_score:
     docid = document_ids.pop(0)
@@ -38,7 +40,7 @@ for obj_id in object_score.iterkeys():
 
 tmp_obj_value = pipe.execute()
 
-# load all object values into a d dict
+# load all object values 
 object_value = dict()
 for obj_id in object_score.iterkeys():
     object_value[obj_id] = tmp_obj_value.pop(0)
@@ -46,3 +48,20 @@ for obj_id in object_score.iterkeys():
 print doc_obj_map
 print object_score
 print object_value
+
+# create a relation between objects in the same document
+obj_rel_map = list()
+for obj_ids in doc_obj_map.itervalues():
+    obj_rel_map.extend([ v for v in combinations(obj_ids,2) ])
+
+print obj_rel_map
+
+# count the relations over all documents
+obj_rel_count = [ [e, obj_rel_map.count(e)] for e in set(obj_rel_map) ]
+
+print obj_rel_count
+
+# finally split relation (edges) and count (weight)
+edges, weight = map(list, zip(*obj_rel_count))
+
+print edges, weight
